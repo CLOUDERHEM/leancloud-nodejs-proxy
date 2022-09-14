b4f26dc06e87ab07d0062564dd56d0778f9b437e
CLOUDERHEM <cloudac@qq.com>
2022-09-14 14:03:46 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const AV = require('leanengine')
+const express = require('express')
+const timeout = require('connect-timeout')
+
+const app = express()
+
+// web socket
+require('express-ws')(app)
+
+// cors
+const cors = require("cors")
+app.use(cors())
+
+app.use(timeout('15s'))
+app.use(AV.express())
+
+// https
+app.enable('trust proxy')
+app.use(AV.Cloud.HttpsRedirect())
+
+
+// proxy
+const proxy = require("http-proxy-middleware").createProxyMiddleware
+app.use("/", proxy(["/"], {
+    headers: {
+        'X-LC-Platform': 'Leancloud'
+    },
+    target: process.env.TARGET,
+    changeOrigin: true,
+    // pathRewrite: {
+    //     "^/api": ""
+    // }
+}))
+
+// error handlers
+app.use(function (err, req, res, _next) {
+    // ignore ws
+    if (req.timedout && req.headers.upgrade === 'websocket') {
+        return
+    }
+
+    let statusCode = err.status || 500
+    if (statusCode === 500) {
+        console.error(err.stack || err)
+    }
+    if (req.timedout) {
+        console.error('Request time: url=%s, timeout=%d', req.originalUrl, err.timeout)
+    }
+
+    res.status(statusCode)
+    let error = {}
+    if (app.get('env') === 'development') {
+        error = err
+    }
+    res.json({
+        errorMessage: err.message,
+        errorCode: statusCode,
+        error: error
+    })
+})
+
+module.exports = app
